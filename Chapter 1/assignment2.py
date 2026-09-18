fca18505ffd3e723c39ce511980583e5ac3678f1
Mir Muntasir Ibne Kashem <anikhorror@gmail.com>
2026-09-18 18:41:46 +0600

    Chapter1

---
+++ Chapter 1/assignment2.py
@@ -0,0 +1,11 @@
+# Question - Take radius as input and calculate the area of a circle
+
+# Formula = Area = π × r²
+
+pi = 3.1416 # pi value
+
+r = float(input("Enter your radius: ")) # Input function user can input value
+
+area = pi * (r * r) #formula of area of circle
+
+print("Area of a circle is -", area) # Area of circle value output
---
+++ Chapter 1/userInput.py
@@ -0,0 +1,7 @@
+#Program to take input from the user 
+
+print("A simple Program to understand Method in python")
+
+name=input("Enter Your Name: ")
+
+print("Your Name Is: ", name)
