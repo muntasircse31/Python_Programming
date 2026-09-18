fce686e461796d412d706bdfd35fa6e648e53620
Mir Muntasir Ibne Kashem <anikhorror@gmail.com>
2026-09-18 12:12:42 +0600

    chapter 0_Assignment_1

---
+++ Chapter 0/assignmen.py
@@ -0,0 +1,19 @@
+# Print Your name 10 times using * this symbol
+print("My name is Muntasir " * 10)
+
+# This is a comment
+
+"""
+this is a multiple line comment
+1. print() is a function. It is used to display output.
+
+2. "" or '' is used to create a string.
+
+3. \n is used to create a new line.
+
+4. * is used for multiplication.
+   It can also be used to repeat a string.
+
+5. + is used for addition.
+   It can also be used for string concatenation.
+"""
