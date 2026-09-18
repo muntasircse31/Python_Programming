9832019f1315f2aa18a9ba45dce35550dc275bc3
Mir Muntasir Ibne Kashem <anikhorror@gmail.com>
2026-09-18 12:07:51 +0600

    Chapter 0 _First Programming

---
+++ Chapter 0/first_programing.py
@@ -0,0 +1,20 @@
+#Print Hello World
+
+print("Hello World!")
+
+
+# Print Your Name 10 Time
+
+
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+print("My Name is Muntasir")
+
+
